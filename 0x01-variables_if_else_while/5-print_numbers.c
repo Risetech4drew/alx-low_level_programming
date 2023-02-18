1838cad7b449d9b06b3c7fc676e1a46ab81ca481
Risetech4drew <kanyinjiandy2@gmail.com>
2023-02-18 11:33:49 -0800

    prints single digit base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * this program prints all single digits of base 10
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int i = 0;
+
+	do
+	{
+		putchar(i + '0');
+		i++;
+	}while (i <= 9);
+	putchar('\n');
+	return (0);
+}

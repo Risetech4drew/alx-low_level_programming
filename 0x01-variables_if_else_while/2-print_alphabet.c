5360029d9391b30016215da369f26d79ec51f0f5
Risetech4drew <kanyinjiandy2@gmail.com>
2023-02-17 23:20:59 -0800

    A C-program that prints the alphabet in lower_case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * this program prints the alphabet in lower_case
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	int i = 97;
+
+	while(i <= 122)
+	{
+		putchar(i);
+		i++;
+	}
+	
+	putchar('\n');
+	return (0);
+}

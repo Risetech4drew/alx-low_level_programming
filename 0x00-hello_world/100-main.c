513393a33a0862f7b923cddd2015e96e3ff2aefc
Risetech4drew <kanyinjiandy2@gmail.com>
2023-02-20 14:43:41 -0800

    assembly code in intel syntax

---
+++ 0x00-hello_world/100-main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * This program prints the string Holberton School to the stdo
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	printf("Holberton School\n");
+	return (0);
+}

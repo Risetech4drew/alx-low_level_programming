12532cfc7a0e68bcd47a67ae67cd59d2555aab08
Risetech4drew <kanyinjiandy2@gmail.com>
2023-02-19 17:13:33 -0800

    prints combinations of numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,17 +16,20 @@ int main(void)
 		j = i+1;
 		while(j < 10)
 		{
+
 			putchar(i + '0');
 			putchar(j + '0');
-			if ( i != 8 || j != 9)
+		
+			if (i < 8)
 			{
+			
 			putchar(',');
 			putchar(' ');
 			}
+
 		j++;
 		}
 		i++;
-		putchar(' ');
 	}
 	return (0);
 }

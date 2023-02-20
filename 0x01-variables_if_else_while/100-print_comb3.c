2db8fc460756e46c7434a0ac785c8658ae9bd3ce
Risetech4drew <kanyinjiandy2@gmail.com>
2023-02-19 16:45:30 -0800

    prints combinations of numbers 0 to 9

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+/**
+ * main - Entry
+ *
+ * This program prints all the possible combination of numbers.
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+	int j = 1;
+
+	while (i < 9)
+	{
+		j = i+1;
+		while(j < 10)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if ( i != 8 || j != 9)
+			{
+			putchar(',');
+			putchar(' ');
+			}
+		j++;
+		}
+		i++;
+		putchar(' ');
+	}
+	return (0);
+}

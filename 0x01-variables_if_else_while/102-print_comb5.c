8148ec912fc3fdbf962c61b6f2a9cefd89546d8e
Risetech4drew <kanyinjiandy2@gmail.com>
2023-02-20 14:03:18 -0800

    prints two digit possible combinations

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,22 +14,22 @@ int main(void)
 
 	while (num1 <= 98)
 	{
-		while (num2 <= 99)
-		{
-				putchar((num1 / 10) + '0');
-				putchar((num1 % 10) + '0');
-				putchar(' ');
-				putchar((num2 / 10) + '0');
-				putchar((num2 % 10) + '0');
-				if (num1 != 98 || num2 != 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			num2++;
-		}
-		num1++;
-		num2 = num1 + 1;
+	while (num2 <= 99)
+	{
+	putchar((num1 / 10) + '0');
+	putchar((num1 % 10) + '0');
+	putchar(' ');
+	putchar((num2 / 10) + '0');
+	putchar((num2 % 10) + '0');
+	if (num1 != 98 || num2 != 99)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	num2++;
+	}
+	num1++;
+	num2 = num1 + 1;
 	}
 	putchar('\n');
 	return (0);

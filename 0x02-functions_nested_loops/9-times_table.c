9d88bf7e3bda03c73a0b0340b7c93794e3d44ae8
Risetech4drew <kanyinjiandy2@gmail.com>
2023-02-22 13:31:03 -0800

    prints 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -33,8 +33,11 @@ void times_table(void)
 				_putchar(tens + '0');
 				_putchar(digit + '0');
 			}
+			if(j < 9)
+			{
 			_putchar(',');
 			_putchar(' ');
+			}
 			j++;
 		}
 		_putchar('\n');

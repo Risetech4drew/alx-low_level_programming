0b941a2fd16da1ab8eae7e1e25063d38ef522c18
Risetech4drew <kanyinjiandy2@gmail.com>
2023-02-22 03:11:06 -0800

    prints the 24_hours of Jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,7 +3,7 @@
  * jack_bauer - Prints every minute minute of the day of jack Jack bauer
  *
  * Description: this function prints every minute of the day jack bauer,
- * 		starting for 00:00 to 23:59.
+ * starting for 00:00 to 23:59.
  */
 void jack_bauer(void)
 {
@@ -12,7 +12,7 @@ void jack_bauer(void)
 
 	while (hour < 24)
 	{
-		while (minute < 59)
+		while (minute < 60)
 		{
 			_putchar(hour / 10 + '0');
 			_putchar(hour % 10 + '0');

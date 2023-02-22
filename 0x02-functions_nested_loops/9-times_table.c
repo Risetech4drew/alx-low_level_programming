703c1b327031b7c7cf3ef6dbd342562d60fe673e
Risetech4drew <kanyinjiandy2@gmail.com>
2023-02-22 13:17:46 -0800

    prints 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table, starting with 0
+ *
+ * Description: Prints a 10x10 grid of products obtained by mutliplying the
+ * numbers 0 to 9 by themselves. each product is printed as a 2-digit number
+ * with leading zeros for single-digit products. The grid is seperated by 
+ * commas and spaces, and each row is terminated by a dollar sign followed by 
+ * a newline character.
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int i = 0;
+	
+	while (i <= 9)
+	{
+		int j = 0;
+		
+		while (j <= 9)
+		{
+			int product = i * j;
+			if(product < 10)
+			{
+				_putchar(' ');
+				_putchar(product + '0');
+			}
+			else 
+			{
+				int tens = product / 10;
+				int digit = product % 10;
+				_putchar(tens + '0');
+				_putchar(digit + '0');
+			}
+			_putchar(',');
+			_putchar(' ');
+			j++;
+		}
+		_putchar('$');
+		_putchar('\n');
+		i++;
+	}
+}
+

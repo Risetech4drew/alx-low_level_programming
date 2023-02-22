915ef94d9cb4edbc322bcc09ed9f89e6b20997c7
Risetech4drew <kanyinjiandy2@gmail.com>
2023-02-22 13:36:30 -0800

    prints 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,8 +4,8 @@
  *
  * Description: Prints a 10x10 grid of products obtained by mutliplying the
  * numbers 0 to 9 by themselves. each product is printed as a 2-digit number
- * with leading zeros for single-digit products. The grid is seperated by 
- * commas and spaces, and each row is terminated by a dollar sign followed by 
+ * with leading zeros for single-digit products. The grid is seperated by
+ * commas and spaces, and each row is terminated by a dollar sign followed by
  * a newline character.
  *
  * Return: void
@@ -13,27 +13,28 @@
 void times_table(void)
 {
 	int i = 0;
-	
 	while (i <= 9)
 	{
 		int j = 0;
 		
-		while (j <= 9)
+	while (j <= 9)
 		{
 			int product = i * j;
-			if(product < 10)
+
+			if (product < 10)
 			{
 				_putchar(' ');
 				_putchar(product + '0');
 			}
-			else 
+			else
 			{
 				int tens = product / 10;
 				int digit = product % 10;
+
 				_putchar(tens + '0');
 				_putchar(digit + '0');
 			}
-			if(j < 9)
+			if (j < 9)
 			{
 			_putchar(',');
 			_putchar(' ');

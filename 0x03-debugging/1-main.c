d753dbac3367d95e2af8800a4ced0dbcf88a6090
Risetech4drew <kanyinjiandy2@gmail.com>
2023-02-23 17:14:03 -0800

    commented out the infinite loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,6 +5,7 @@
 * Description: the main function causes an infinite loop,
 * but the loop is commented out to avoid it.
 *
+* Return: Always 0 (success)
 */
 int main(void)
 
@@ -18,7 +19,6 @@ i = 0;
 /*{*/
 /*putchar(i);*/
 /*}*/
-
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }

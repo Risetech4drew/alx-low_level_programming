b2ba37cb3b8e4ad4ef142ba34d1b4ef1218f0288
Risetech4drew <kanyinjiandy2@gmail.com>
2023-02-25 04:39:18 -0800

    prints triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,30 +10,30 @@
  */
 void print_triangle(int size)
 {
-	int i;
+int i;
 
-	if (size < 0)
-	{
-		_putchar('\n');
-		return;
-	}
-	i = 1;
-	while (i <= size)
-	{
-		int j = 1;
-		while (j <= size - i)
-		{
-			_putchar(' ');
-			j++;
-		}
-		j = 1;
-		while (j <= i)
-		{
-		_putchar('#');
-		j++;
-		}	
-		_putchar('\n');
-		i++;
-	}
-	
+if (size < 0)
+{
+_putchar('\n');
+return;
+}
+i = 1;
+while (i <= size)
+{
+int j = 1;
+
+while (j <= size - i)
+{
+_putchar(' ');
+j++;
+}
+j = 1;
+while (j <= i)
+{
+_putchar('#');
+j++;
+}
+_putchar('\n');
+i++;
+}
 }

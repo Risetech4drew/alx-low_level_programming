e7d0ebf22b3c3fd3217517867c42d1eaed77c6b5
Risetech4drew <kanyinjiandy2@gmail.com>
2023-03-10 14:25:00 -0800

    prints thenumber of argumnets

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
+/**
+ * main - prints the number of arguments passed
+ *
+ * @argc: argument counter
+ * @argv: argumnet vector 
+ *
+ * Return: number of argumnets
+ */
 int main(int argc, char **argv __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
